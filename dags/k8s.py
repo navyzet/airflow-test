fd99f8f0afc8a734e05e1d67ee21dcb0965fd58b
Nikita Lipchik <lipchik.n@gmail.com>
2024-04-28 15:58:32 +0300

    Get tag variable

--- dags/k8s.py
+++ dags/k8s.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.models import Variable
 from airflow.utils.dates import days_ago
+import os
 
 default_args = {
     'owner': 'your_name',
@@ -24,7 +25,8 @@ task = KubernetesPodOperator(
     task_id='run_kubernetes_pod_task',
     name='run-k8s-pod-task',
     namespace='default',  # Change to your desired Kubernetes namespace
-    image='ubuntu:latest',  # Specify the Docker image to run in the pod
+    image='ubuntu',  # Specify the Docker image to run in the pod
+    tag = os.environ.get('MY_TAG', 'latest')
     cmds=["echo"],
     # arguments=["Hello, Airflow!"],  # Command and arguments to run in the pod
     arguments=pod_args,  # Command and arguments to run in the pod

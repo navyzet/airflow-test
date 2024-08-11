4a5d04e9fd789e7da7a3a20b5ec77ea33ace8de6
Nikita Lipchik <lipchik.n@gmail.com>
2024-08-11 19:56:16 +0300

    Update k8-next.py

--- dags/k8-next.py
+++ dags/k8-next.py
@@ -31,6 +31,7 @@ task = KubernetesPodOperator(
     labels={"app": "myapp"},
     get_logs=True,
     dag=dag,
+    kubernetes_conn_id="old",
     # service_account_name='airflow-service-account',
 )
 

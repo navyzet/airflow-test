b68d8ec98bd5d8c33d4c0c323f43f0debf3e8c20
Nikita Lipchik <lipchik.n@gmail.com>
2024-08-29 13:23:44 +0300

    Update generator.py

--- dags/generator.py
+++ dags/generator.py
@@ -1,1 +1,24 @@
+import pendulum
 
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+DAGS_COUNT = 350
+
+for i in range(DAGS_COUNT):
+    id = f"debug_{i}"
+    with DAG(
+        id,
+        schedule="@daily",
+        start_date=pendulum.today("UTC").add(days=-2),
+        catchup=False,
+        max_active_runs=1,
+        max_active_tasks=1,
+    ) as dag:
+        PythonOperator(
+            task_id=id,
+            python_callable=print,
+            op_args=(f"Hello from debug {i}",)
+        )
+
+    globals()[id] = dag
